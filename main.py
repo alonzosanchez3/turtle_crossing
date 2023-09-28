6ff71fad2a926403e5edeb7cca5c74d4d9b23f81
Alonzo Sanchez <alonzosanchez3@gmail.com>
2023-09-28 09:23:13 -0500

    implemented car_manager class

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from turtle import Turtle, Screen
 from custom_turtle import CustomTurtle
 import time
+from car_manager import CarManager
 
 screen = Screen()
 screen.setup(height=500, width=1000)
 custom_turtle = CustomTurtle()
+car_manager = CarManager()
 screen.tracer(0)
 screen.listen()
 screen.onkey(custom_turtle.move, 'w')
@@ -14,6 +16,13 @@ game_is_on = True
 while game_is_on:
   screen.update()
   time.sleep(0.1)
+  car_manager.create_car()
+  car_manager.move_cars()
+
+  for car in car_manager.all_cars:
+    if(car.distance(custom_turtle) < 20):
+      game_is_on = False
+
 
 
 screen.exitonclick()
